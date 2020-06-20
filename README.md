e6d517028ebe82b0dfd73ee0c85a0a801f3f8cf1
Aditya Agarwal <aditya81070@gmail.com>
2020-06-20 21:36:48 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Github actions learn
+
+This npm package is used to learn about the github actions for automatic npm publish and github tag release.
+
+It is a sample package that has some buggy code to check semantic version release for the npm using github actions.
---
+++ index.js
@@ -0,0 +1,22 @@
+/**
+ * returns the sum of two numbers
+ * @param a number
+ * @param b number
+ * @returns number
+ */
+const sum = (a = 0, b = 0) => a + b;
+
+// TODO: fix the bugs
+const subtract = (a, b) => a + b;
+
+// TODO: add new feature for multiplication
+
+//const multiply = (a,b) => a*b;
+
+// TODO: breaking change - remove subtract function and use third argument in sum function
+
+// const sum = (a= 0, b = 0, isSubtract) => isSubtract ? a - b: a + b;
+
+module.exports = sum;
+
+module.exports.subtract = subtract;
